ea710b0e31a70711fcf4f8b9edea0ac509b11247
Sugiuro <sugiuro.dev@gmail.com>
2022-04-02 21:55:31 +0200

    Change hitbox sprite

---
+++ Assets/Scripts/Player/PlayerBulletBehavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletBehavior : MonoBehaviour
+{
+    public float speed = 5f;
+    public float ttl = 1f;
+    public int damage = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        Destroy(this.gameObject, ttl);
+    }
+}
