1e456df609746c312da73a576c72178da9a108a6
Sugiuro <sugiuro.dev@gmail.com>
2023-01-08 01:35:32 +0100

    [api] connect routes to Firestore

--- api/deploy.cmd
+++ api/deploy.cmd
@@ -1,7 +1,7 @@
 @REM Deploys the API to Cloud Run
 @REM Please setup the following variables before running
 
-@echo off 
+@echo off
 set gcp_project_id=sugi-dev-sandbox
 set service_account=definitely-not-touhou@sugi-dev-sandbox.iam.gserviceaccount.com
 
@@ -12,4 +12,6 @@ CALL gcloud builds submit --tag eu.gcr.io/%gcp_project_id%/definitely-not-touhou
 CALL gcloud run deploy definitely-not-touhou-api^
  --image eu.gcr.io/%gcp_project_id%/definitely-not-touhou-api^
  --region europe-west1 --allow-unauthenticated --platform managed^
- --service-account %service_account%
+ --service-account %service_account%^
+ --set-env-vars^
+ DEFINITELY_NOT_TOUHOU_FSDB_BASE_COLLECTION=projects/definitelyNotTouhou,GCP_PROJECT=%gcp_project_id%
--- api/main.py
+++ api/main.py
@@ -1,9 +1,26 @@
+import os
 from typing import List, Optional
 
-# import uvicorn
-from fastapi import FastAPI, Path
+import firebase_admin
+import google.cloud.firestore_v1 as fsv1
+from fastapi import FastAPI, HTTPException, Path
 from fastapi.middleware.cors import CORSMiddleware
+from firebase_admin import credentials, firestore
 from pydantic import BaseModel
+
+ROOT_COLLECTION = os.environ.get("DEFINITELY_NOT_TOUHOU_FSDB_BASE_COLLECTION", "")
+SCORE_COLLECTION_PATH = f"{ROOT_COLLECTION}/scores" if ROOT_COLLECTION else "scores"
+PENDING_SCORE_COLLECTION_PATH = (
+    f"{ROOT_COLLECTION}/pendingScoreUploads"
+    if ROOT_COLLECTION
+    else "pendingScoreUploads"
+)
+
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(
+    cred, {"projectId": os.environ.get("GCP_PROJECT", "sugi-dev-sandbox")}
+)
+db: fsv1.Client = firestore.client()
 
 tags_metadata = [
     {
@@ -54,7 +71,29 @@ class ScoreModel(BaseModel):
     tags=["scoring"],
 )
 def publish_score(*, score_id: str = Path(), payload: PublishScorePayload):
-    return f"publish-score {score_id} {payload.username}", 200
+    tmp_score_doc = (
+        db.collection(PENDING_SCORE_COLLECTION_PATH).document(score_id).get()
+    )
+
+    tmp_score_data = tmp_score_doc.to_dict()
+
+    if not tmp_score_data:
+        raise HTTPException(status_code=404, detail="Score not found")
+
+    fs_transaction = db.transaction()
+    fs_transaction.delete(
+        db.collection(PENDING_SCORE_COLLECTION_PATH).document(score_id)
+    )
+    fs_transaction.create(
+        db.collection(SCORE_COLLECTION_PATH).document(score_id),
+        tmp_score_data
+        | {
+            "createdTime": firestore.firestore.SERVER_TIMESTAMP,
+            "author": payload.username,
+        },
+    )
+    fs_transaction.commit()
+    return 200
 
 
 @app.post(
@@ -64,21 +103,36 @@ def publish_score(*, score_id: str = Path(), payload: PublishScorePayload):
     tags=["scoring"],
 )
 def submit_tmp_score(submitted_score: SubmitScorePayload):
-    top_scores = get_top_scores()
-    top_scores.append(
-        ScoreModel(
-            author="",
-            score=submitted_score.score,
-            secondsSurvived=submitted_score.seconds_survived,
-            tmpScoreId="tmp-id",  # TODO: get from DB upload
-        )
+    limit = 10
+    top_scores = get_top_scores(limit=limit)
+
+    tmp_doc_id = db.collection(PENDING_SCORE_COLLECTION_PATH).document().id
+    score = ScoreModel(
+        author="",
+        score=submitted_score.score,
+        secondsSurvived=submitted_score.seconds_survived,
+        tmpScoreId=tmp_doc_id,
     )
-    top_scores = sorted(top_scores, key=lambda x: x.secondsSurvived, reverse=True)[:10]
+    top_scores.append(score)
+    top_scores = sorted(top_scores, key=lambda x: x.secondsSurvived, reverse=True)[
+        :limit
+    ]
+    if score in top_scores:
+        db.collection(PENDING_SCORE_COLLECTION_PATH).document(tmp_doc_id).set(
+            {x: y for x, y in score.dict().items() if x in ("score", "secondsSurvived")}
+            | {"createdTime": firestore.firestore.SERVER_TIMESTAMP}
+        )
     return top_scores
 
 
 @app.get("/get-top-scores", response_model=List[ScoreModel], tags=["scoring"])
-def get_top_scores(username: Optional[str] = None, limit: Optional[int] = 10):
+def get_top_scores(username: Optional[str] = None, limit: int = 10):
+    query = db.collection(SCORE_COLLECTION_PATH)
+    if username:
+        query = query.where("author", "==", username)
+    docs = query.order_by("secondsSurvived", direction="DESCENDING").limit(limit).get()
+
+    return [ScoreModel(**data) for data in (doc.to_dict() for doc in docs) if data]
     return [
         ScoreModel(author="moi", score=10, secondsSurvived=3),
         ScoreModel(author="toi", score=1031, secondsSurvived=13),
@@ -91,7 +145,6 @@ def get_top_scores(username: Optional[str] = None, limit: Optional[int] = 10):
         ScoreModel(author="bob", score=1110, secondsSurvived=11),
         ScoreModel(author="bob", score=1110, secondsSurvived=3),
     ]
-    # return f"get_top_scores {username}", 200
 
 
 if __name__ == "__main__":
